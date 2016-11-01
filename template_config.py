04dcc1c93f7d876b4cdf67a68260a271b1319b5e
rmasse <roman.masse@gmail.com>
2016-11-01 16:54:10 +0100

    First commit

---
+++ template_config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+#
+# Copy this file and rename it to config.py
+# It must contain yours correct USER id and PASS token
+# You can get these values at:
+#
+# https://mobile.free.fr/moncompte/
+#
+# Make sure sms notification option is correctly activated !
+#
+# Example :
+
+USER = 12345678
+PASS = "SfRdsfRFzdfHPa"
